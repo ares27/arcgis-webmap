a71db7ea6a6e772ccc17c2b7c85093ca6a2a8b2b
ares27 <jehomasb@gmail.com>
2021-08-25 21:21:54 +0200

    added weather indicator to webmap toast.

--- js/custom/constants.js
+++ js/custom/constants.js
@@ -2,6 +2,8 @@ const toastHTMLElement = document.getElementById("EpicToast");
 const toastBody = document.querySelector(".toast-body");
 const toastAddress = document.querySelector("#address");
 const toastCoords = document.querySelector("#coords");
+const toastTemperature = document.querySelector("#temperature");
+const toastWeatherImg = document.querySelector("#weather-img");
 
 let layersArray = [];
 layersArray["armedConflictLayer"] = {
